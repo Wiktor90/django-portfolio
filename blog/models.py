5b0cabb7ab57286421b3855c1da18e10a72dbaec
Wiktor90 <w-witkowski90@o2.pl>
2020-08-10 15:35:53 +0200

    Add new tamplates / css / fontello. Add img to model. Display posts in Django loop.

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    description = models.TextField(default="description", max_length=400)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(default='default.jpg', upload_to='post_pics')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -15,4 +18,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        if img.height >300 or img. width >300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
