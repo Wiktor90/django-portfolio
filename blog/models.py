9af5bebdf1824f876e26631f36aba13f0e0a9364
Wiktor90 <w-witkowski90@o2.pl>
2020-08-14 10:50:27 +0200

    Add URLField to model Post. Migration. Add Repo button in post_details.html

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(default='default.jpg', upload_to='post_pics')
+    url = models.URLField(max_length=400, default="")
 
     def publish(self):
         self.published_date = timezone.now()
